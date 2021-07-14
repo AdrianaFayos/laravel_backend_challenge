eccc5a9a479633813270c86ef77cb2f630c5db88
AdrianaFayos <adrifayosperis@gmail.com>
2021-07-14 17:35:15 +0200

    game fillable added

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,8 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     // use HasFactory;
+    protected $fillable = [
+        'title',
+        'thumbnail_url',
+        'url',
+    ];
 
     public function party (){
         return $this -> belongsTo(Party::class);
     }
+    
 }
