d01d70495e25d8d805ae0c0c1b91dd94cb79d977
AdrianaFayos <adrifayosperis@gmail.com>
2021-07-14 17:35:34 +0200

    users and game routes added to api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\GameController;
 use App\Http\Controllers\PassportAuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +21,9 @@ Route::post('register', [PassportAuthController::class, 'register']);
 Route::post('login', [PassportAuthController::class, 'login']);
 
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function(){
+    Route::resource('users', UserController::class);
+    Route::resource('games', GameController::class);
+    
 });
+
